29ddef7b9f6eae4a76ba63d2c735c516f98777e6
3amo-magdy <amoramgadymahmoud@gmail.com>
2022-04-02 09:15:10 +0200

    gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, ViewChild } from "@angular/core";
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { IViewable } from "./SFGComponents/IViewable";
 import { link } from "./SFGComponents/link";
 import { mode } from "./SFGComponents/mode";
@@ -7,252 +7,252 @@ import { node } from "./SFGComponents/node";
 import { IdGenerator } from "./SFGComponents/IdGenerator";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  nodes!:node[];
-  links!:link[];
-  MODE!:mode;
-  selected!:IViewable;
-  lastSelectednode!:node;
+  nodes!: node[];
+  links!: link[];
+  MODE!: mode;
+  selected!: IViewable;
+  lastSelectednode!: node;
 
-  generator:IdGenerator;
+  generator: IdGenerator;
   //front:
-  win!:Window;
-  @ViewChild('canvas')
+  win!: Window;
+  @ViewChild("canvas")
   Ecanvas!: ElementRef;
-  canvas!:SVGElement;
-  axis_height:number=innerHeight/2;
-  axis_pointer!:number;
-  NODEWIDTH=40;
+  canvas!: SVGElement;
+  axis_height: number = innerHeight / 2;
+  axis_pointer!: number;
+  NODEWIDTH = 40;
   ArrowWidth = 16; //pixels
   ArrowHeight = 10; //pixels
-  constructor(){
-    this.nodes=[];
-    this.links=[];
+  constructor() {
+    this.nodes = [];
+    this.links = [];
     this.generator = new IdGenerator();
     this.win = window;
     this.NODEWIDTH = 40;
     this.axis_pointer = 0;
   }
   ngOnInit() {
-
-    this.MODE=mode.linking;
+    this.MODE = mode.linking;
   }
-  ngAfterViewInit(){
-    document.addEventListener('keydown',this.KeyDown.bind(this),false);
-    document.addEventListener('resize',this.update_axis.bind(this),false);
-    document.addEventListener('scroll',this.update_axis.bind(this),false);
-    document.addEventListener('mouseenter',this.update_axis.bind(this),false);
-    document.addEventListener('mousedown',this.update_axis.bind(this),false);
-    document.addEventListener('mouseleave',this.update_axis.bind(this),false);
+  ngAfterViewInit() {
+    document.addEventListener("keydown", this.KeyDown.bind(this), false);
+    document.addEventListener("resize", this.update_axis.bind(this), false);
+    document.addEventListener("scroll", this.update_axis.bind(this), false);
+    document.addEventListener("mouseenter", this.update_axis.bind(this), false);
+    document.addEventListener("mousedown", this.update_axis.bind(this), false);
+    document.addEventListener("mouseleave", this.update_axis.bind(this), false);
 
-
-
-    this.canvas = (this.Ecanvas.nativeElement as SVGElement);
+    this.canvas = this.Ecanvas.nativeElement as SVGElement;
   }
-  update_axis(){
-    this.axis_height=window.innerHeight/2;
+  update_axis() {
+    this.axis_height = window.innerHeight / 2;
   }
-  getOffset(){
-    if(this.canvas){
-      var bound =this.canvas.getBoundingClientRect();
-      var html =document.documentElement;
-      return{
-        top:bound.top+window.pageYOffset-html.clientTop,
-        left:bound.left+window.pageXOffset-html.clientLeft
-      }
-    }
-    else{
-      return{
-        top:20,
-        left:20
-      }
-
+  getOffset() {
+    if (this.canvas) {
+      var bound = this.canvas.getBoundingClientRect();
+      var html = document.documentElement;
+      return {
+        top: bound.top + window.pageYOffset - html.clientTop,
+        left: bound.left + window.pageXOffset - html.clientLeft,
+      };
+    } else {
+      return {
+        top: 20,
+        left: 20,
+      };
     }
   }
-  selectingAlink():boolean{
+  selectingAlink(): boolean {
     return this.selected instanceof link;
   }
 
-  select(v:IViewable){
+  select(v: IViewable) {
     console.log("selected :");
     console.log(v);
-    this.selected=v;
-    if(v instanceof node){
-      this.MODE=mode.selectingNode;
+    this.selected = v;
+    if (v instanceof node) {
+      this.MODE = mode.selectingNode;
       this.lastSelectednode = v;
-    }
-    else{
-      this.MODE=mode.selectingLink;
+    } else {
+      this.MODE = mode.selectingLink;
     }
   }
 
-  delete(v:IViewable){
+  delete(v: IViewable) {
     let arr;
-    switch(this.MODE){
+    switch (this.MODE) {
       case mode.selectingNode:
-          this.deleteAllLinks(v as node);
-          let index =  this.removefromarr(this.nodes,v);
-          for(;index<this.nodes.length;index++){
-            this.nodes[index].DecrementX();
-          }
-          this.axis_pointer--;
-          break;
+        this.deleteAllLinks(v as node);
+        let index = this.removefromarr(this.nodes, v);
+        for (; index < this.nodes.length; index++) {
+          this.nodes[index].DecrementX();
+        }
+        this.axis_pointer--;
+        break;
       case mode.selectingLink:
-            this.disconnectBothnodes(v as link);
-            this.removefromarr(this.links,v);
-            break;
+        this.disconnectBothnodes(v as link);
+        this.removefromarr(this.links, v);
+        break;
       default:
-            return;
+        return;
     }
   }
 
-  private removefromarr(arr:IViewable[],v:IViewable):number{
-    for(var i=0;i<arr.length;i++){
-      if(v==arr[i]){
-        arr.splice(i,1);
+  private removefromarr(arr: IViewable[], v: IViewable): number {
+    for (var i = 0; i < arr.length; i++) {
+      if (v == arr[i]) {
+        arr.splice(i, 1);
         return i;
       }
     }
     return -1;
   }
-  deleteAllLinks(v:node){
+  deleteAllLinks(v: node) {
     for (let index = 0; index < this.links.length; index++) {
       let l = this.links[index];
-      if (l.from==v||l.to==v){
+      if (l.from == v || l.to == v) {
         this.disconnectBothnodes(l);
-        this.links.splice(index,1);
+        this.links.splice(index, 1);
         index--;
       }
     }
   }
-  disconnectBothnodes(l:link){
-     let a:node = l.from;
-     let b:node = l.to;
-     this.removefromarr(a.Out,b);
-     this.removefromarr(b.In,a);
-     this.removefromarr(a.OutLinks,l);
-     this.removefromarr(b.InLinks,l);
+  disconnectBothnodes(l: link) {
+    let a: node = l.from;
+    let b: node = l.to;
+    this.removefromarr(a.Out, b);
+    this.removefromarr(b.In, a);
+    this.removefromarr(a.OutLinks, l);
+    this.removefromarr(b.InLinks, l);
   }
-  Mode_creatingNode(){
-    this.MODE=mode.creatingNode;
+  Mode_creatingNode() {
+    this.MODE = mode.creatingNode;
   }
-  resetSelection(e:MouseEvent){
-    if(this.MODE==mode.creatingNode){
-      console.log("just created a node")
+  resetSelection(e: MouseEvent) {
+    if (this.MODE == mode.creatingNode) {
+      console.log("just created a node");
       this.addnode();
     }
   }
-  addnode(){
-      let id = this.generator.generate();
-      let newnode=new node(id,this.axis_pointer,this.axis_height);
-      this.nodes.push(newnode);
-      this.axis_pointer++;
+  addnode() {
+    let id = this.generator.generate();
+    let newnode = new node(id, this.axis_pointer, this.axis_height);
+    this.nodes.push(newnode);
+    this.axis_pointer++;
   }
-  islinked(from:node,to:node):boolean{
-    if(from.Out.includes(to)){
+  islinked(from: node, to: node): boolean {
+    if (from.Out.includes(to)) {
       return true;
     }
     return false;
   }
-  Link(from:node,to:node):void{
-    if(this.islinked(from,to)){
+  Link(from: node, to: node): void {
+    if (this.islinked(from, to)) {
       window.alert("already linked");
       return;
     }
-    var newLink = new link(from,to);
+    var newLink = new link(from, to);
     from.OutLinks.push(newLink);
     to.InLinks.push(newLink);
     from.Out.push(to);
     to.In.push(from);
     this.links.push(newLink);
   }
-  mouseUp(v:IViewable){
+  mouseUp(v: IViewable) {
     console.log("mouse up:");
     console.log(v);
     console.log("links:");
     console.log(this.links);
 
-    if(this.MODE==mode.selectingNode&& v instanceof node){
-        this.Link(this.lastSelectednode,v as node);
-    }
-    else{
+    if (this.MODE == mode.selectingNode && v instanceof node) {
+      this.Link(this.lastSelectednode, v as node);
+    } else {
       //do nothing
     }
   }
-  KeyDown(e:KeyboardEvent){
+  KeyDown(e: KeyboardEvent) {
     let keyName = e.key;
-    if(e.key==='Delete'||e.key==="D"||e.key==="d"){
+    if (e.key === "Delete" || e.key === "D" || e.key === "d") {
       this.delete(this.selected);
     }
-    if(e.key==='n'||e.key==="N"){
-      this.MODE=mode.creatingNode;
+    if (e.key === "n" || e.key === "N") {
+      this.MODE = mode.creatingNode;
     }
   }
 
-  getItem(arr:IViewable[], id:string ){
-    for (var i=0;i<arr.length;i++) {
-      if(arr[i].id===id){
+  getItem(arr: IViewable[], id: string) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i].id === id) {
         return arr[i];
       }
     }
-    throw new Error("couldn't find the id :"+id);
+    throw new Error("couldn't find the id :" + id);
   }
-  evaluate_curve(l: link):string{
-    var res:string;
+  evaluate_curve(l: link): string {
+    var res: string;
     let x1 = this.evaluate_x(l.from);
     let x2 = this.evaluate_x(l.to);
     //self loop
-    if(x1==x2){
-      return `M ${x1-this.NODEWIDTH/3} ${this.axis_height} T ${x1-this.NODEWIDTH/3} ${this.axis_height-(this.NODEWIDTH)} ${x1+this.NODEWIDTH/3} ${this.axis_height-(this.NODEWIDTH)} ${x2+this.NODEWIDTH/3} ${this.axis_height}`;
-      
+    if (x1 == x2) {
+      return `M ${x1 - this.NODEWIDTH / 3} ${this.axis_height} T ${
+        x1 - this.NODEWIDTH / 3
+      } ${this.axis_height - this.NODEWIDTH} ${x1 + this.NODEWIDTH / 3} ${
+        this.axis_height - this.NODEWIDTH
+      } ${x2 + this.NODEWIDTH / 3} ${this.axis_height}`;
     }
-    res = `M ${x1} ${this.axis_height} Q ${(x1+x2)/2} ${this.axis_height-(x2-x1)/2} ${x2} ${this.axis_height}`;
+    res = `M ${x1} ${this.axis_height} Q ${(x1 + x2) / 2} ${
+      this.axis_height - (x2 - x1) / 2
+    } ${x2} ${this.axis_height}`;
     return res;
   }
-  evaluate_x(node: node):number{
-    var res = this.NODEWIDTH/2;
-    if(!(this.nodes.length==0)){
-      res = (node.X*window.innerWidth/this.nodes.length);
-    if(this.nodes.length>=2&&this.nodes[this.nodes.length-1]==node){
-      res-=this.NODEWIDTH/2;
+  evaluate_x(node: node): number {
+    var res = this.NODEWIDTH / 2;
+    if (!(this.nodes.length == 0)) {
+      res = (node.X * window.innerWidth) / this.nodes.length;
+      if (this.nodes.length >= 2 && this.nodes[this.nodes.length - 1] == node) {
+        res -= this.NODEWIDTH / 2;
+      } else if (this.nodes[0] == node) {
+        res += this.NODEWIDTH / 2;
+      }
     }
-    else if(this.nodes[0]==node){
-      res+=this.NODEWIDTH/2;
-    }
-  }
     return res;
   }
-  evaluate_curve_midPoint_y(l:link){
-    let x0= this.evaluate_x(l.from);
+  evaluate_curve_midPoint_y(l: link) {
+    let x0 = this.evaluate_x(l.from);
     let x2 = this.evaluate_x(l.to);
-    if(x0==x2){
-      return this.axis_height - 2/3 *this.NODEWIDTH
+    if (x0 == x2) {
+      return this.axis_height - (2 / 3) * this.NODEWIDTH;
     }
-    let y1 = this.axis_height-(x2-x0)/2;
-    return 0.5 * this.axis_height + 0.5 * y1 ;
+    let y1 = this.axis_height - (x2 - x0) / 2;
+    return 0.5 * this.axis_height + 0.5 * y1;
   }
-  evaluate_arrow(l:link):string{
-    let res ="";
+  evaluate_arrow(l: link): string {
+    let res = "";
     let ArrowWidth = this.ArrowWidth;
     let mid_point_y = this.evaluate_curve_midPoint_y(l);
-    let x1= this.evaluate_x(l.from);
+    let x1 = this.evaluate_x(l.from);
     let x2 = this.evaluate_x(l.to);
-    if(x1 == x2){
+    if (x1 == x2) {
       return "";
     }
-    let mid_point_x = (x2+x1)/2;
-    if(x1>x2){
-      ArrowWidth*=-1;
+    let mid_point_x = (x2 + x1) / 2;
+    if (x1 > x2) {
+      ArrowWidth *= -1;
     }
-    return `${mid_point_x+1/3*ArrowWidth} ${mid_point_y} ${mid_point_x-ArrowWidth*2/3} ${mid_point_y-this.ArrowHeight*1/3} ${mid_point_x-ArrowWidth*2/3} ${mid_point_y+this.ArrowHeight*1/3}`
+    return `${mid_point_x + (1 / 3) * ArrowWidth} ${mid_point_y} ${
+      mid_point_x - (ArrowWidth * 2) / 3
+    } ${mid_point_y - (this.ArrowHeight * 1) / 3} ${
+      mid_point_x - (ArrowWidth * 2) / 3
+    } ${mid_point_y + (this.ArrowHeight * 1) / 3}`;
   }
-  editGain(e : any){
+  editGain(e: any) {
     (this.selected as link).gain = e.target.value;
     e.target.value = "";
   }
 }
-
