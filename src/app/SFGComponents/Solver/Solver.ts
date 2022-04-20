730119e245c3092193a5c5dfc4cf83f54aee5c9f
yarahossamboo <yarahossam222@gmail.com>
2022-04-20 09:54:56 +0200

    buggy fixed

--- src/app/SFGComponents/Solver/Solver.ts
+++ src/app/SFGComponents/Solver/Solver.ts
@@ -1,13 +1,10 @@
 import { node } from "../node";
 import { ISolver } from "./ISolver";
 
-let gainStack: number[] = [];
 const nodeMap = new Map<string, number>();
 export class Solver implements ISolver{
     path: string[] = []
-    gain: number = 1;
     isVisited: boolean[] = [];
-    temp!: string;
     forwardPaths!: pathInfo[];
     mapNodes(nodes: node[]) {
         for(let i=0; i<nodes.length; i++) 
@@ -30,7 +27,7 @@ export class Solver implements ISolver{
         let u = nodeMap.get(src.name);
         let v = nodeMap.get(dest.name);
         this.DFS(nodes, u!, v!);
-        console.log("weeeeeee ", this.forwardPaths, gainStack)
+        console.log("weeeeeee ", this.forwardPaths)
     }    
 
     DFS(nodes: node[], u: number, dest: number) {
@@ -39,7 +36,8 @@ export class Solver implements ISolver{
             this.path.push(this.getByValue(u)!);
             var temp = JSON.parse(JSON.stringify(this.path));
             newPath.path = temp;
-            newPath.gain = this.gain;
+            newPath.gain = this.getGain(nodes, newPath)
+         //   newPath.gain = totalGain;
             this.forwardPaths.push(newPath);
             this.path.pop();
             // return; ?????? check
@@ -51,16 +49,36 @@ export class Solver implements ISolver{
             for(let i=0; i<nodes[u].OutLinks.length; i++) {
                 let index = nodes[u].OutLinks[i];
                 if(this.isVisited[nodeMap.get(index.to.name)!]  == false) { 
-                    this.gain = this.gain * index.gain;
+                   // totalGain = totalGain * index.gain
                     this.DFS(nodes,nodeMap.get(nodes[u].Out[i].name)!, dest);
                 }
-                this.gain = this.gain / index.gain;
+            // else{
+            //
+            //  totalGain = totalGain / index.gain
+            // }
+            // this.gain = this.gain / index.gain;
             }
         this.path.pop();
         this.isVisited[u] = false;
         }
     }
+
+
+    getGain(nodes: node[], path: pathInfo) {
+        var gain = 1;
+       for(let j=0; j<path.path.length-1; j++) {
+        let x  = nodeMap.get(path.path[j]);
+        let y = nodeMap.get(path.path[j+1]);
+            for(let i=0; i<nodes[x!].OutLinks.length; i++) {
+                if(nodes[x!].OutLinks[i].from == nodes[x!] && nodes[x!].OutLinks[i].to == nodes[y!]) {
+                    gain = gain * nodes[x!].OutLinks[i].gain;
+                }
+            }
+        }
+        return gain;
+    }
 }
+
 class pathInfo {
     path: string[];
     gain: number;
